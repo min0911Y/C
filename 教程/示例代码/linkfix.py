6d0e0c795dc382ababada3dc7f0cbdff17a19982
LY-Xiang <3047563007@qq.com>
2024-08-12 10:46:31 +0800

    更新 linkfix.py

--- 教程/示例代码/linkfix.py
+++ 教程/示例代码/linkfix.py
@@ -1,7 +1,8 @@
+# coding=utf-8
 from os import walk
-from os.path import join as pjoin
+from os.path import join
 for path, _, files in walk('.'):
     for file in files:
         if not file.endswith('.md'):
-            with open(pjoin(path, file) + '.md', 'w') as f:
+            with open(join(path, file) + '.md', 'w') as f:
                 f.write(f"[{file}](./{file} ':include')")
